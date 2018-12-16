c15fc4172434432df90d1af14e43d18c1f8601af
augrinn <augrinn@gmail.com>
2018-12-16 16:39:13 +0300

    lesson3

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 
 import styled from 'styled-components'
+
+import Slider from './Slider'
 
 const Repair = styled.div`
 	color: #ffffff;
@@ -54,7 +56,7 @@ class Main extends React.Component {
 						</CallBtn>
 					</Col>
 					<Col lg={6} Offset={1}>
-
+						<Slider />
 					</Col>
 				</Row>
 			)
